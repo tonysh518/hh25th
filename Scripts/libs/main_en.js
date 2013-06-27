6e2a387d488b7cc6b4080c934152426b78a031dc
tonysh518 <tony@fuel-it-up.com>
2013-06-27 15:54:27 +0800

    updated

--- Scripts/libs/main_en.js
+++ Scripts/libs/main_en.js
@@ -1,61 +1,61 @@
-$(documcnt).ready(function(){
+$(document).ready(function(){
     $('.headnav a').eq(0).bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Link', 'Index', 'cn']);
+        _gaq.push(['_trackEvent', 'Link', 'Index', 'en']);
     });
 
     $('.headnav a').eq(1).bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Link', 'Winners', 'cn']);
+        _gaq.push(['_trackEvent', 'Link', 'Winners', 'en']);
     });
 
     $('.headnav a').eq(2).bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Link', '25Weeks', 'cn']);
+        _gaq.push(['_trackEvent', 'Link', '25Weeks', 'en']);
     });
 
     $('.headnav a').eq(3).bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Link', '25Years', 'cn']);
+        _gaq.push(['_trackEvent', 'Link', '25Years', 'en']);
     });
 
     $('.langnav a').eq(0).bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Link', 'Lang', 'en']);
+        _gaq.push(['_trackEvent', 'Link', 'Lang', 'cn']);
     });
 
     $('.langnav a').eq(1).bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Follow', 'Weibo', 'cn']);
+        _gaq.push(['_trackEvent', 'Follow', 'Weibo', 'en']);
     });
 
     $('.share a.share_mail').bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Share', 'Email', 'cn']);
+        _gaq.push(['_trackEvent', 'Share', 'Email', 'en']);
     });
 
     $('.share a.share_weibo').bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Link', 'Weibo', 'cn']);
+        _gaq.push(['_trackEvent', 'Link', 'Weibo', 'en']);
     });
 
     $('.share a.button3').bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Link', 'LearnMore', 'cn']);
+        _gaq.push(['_trackEvent', 'Link', 'LearnMore', 'en']);
     });
 
     $('.join a.button1').bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Link', 'Join', 'cn']);
+        _gaq.push(['_trackEvent', 'Link', 'Join', 'en']);
     });
 
     $('.join a.button2').bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Follow', 'Weibo', 'cn']);
+        _gaq.push(['_trackEvent', 'Follow', 'Weibo', 'en']);
     });
 
     $('.grouplink div').eq(0).find('a').bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Link', '25years', 'cn']);
+        _gaq.push(['_trackEvent', 'Link', '25years', 'en']);
     });
 
     $('.grouplink div').eq(1).find('a').bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Link', 'Winners', 'cn']);
+        _gaq.push(['_trackEvent', 'Link', 'Winners', 'en']);
     });
 
     $('.grouplink div').eq(2).find('a').bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Link', '25weeks', 'cn']);
+        _gaq.push(['_trackEvent', 'Link', '25weeks', 'en']);
     });
 
     $('.term-condition-title a').bind('click',function(){
-        _gaq.push(['_trackEvcnt', 'Link', 'Terms', 'cn']);
+        _gaq.push(['_trackEvent', 'Link', 'Terms', 'en']);
     });
 });
